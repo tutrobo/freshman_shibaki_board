9028e087954bb14796be3e1d4a7f633cc39787b1
Sato Tomoki <mega33420@gmail.com>
2025-04-05 03:53:49 +0900

    まっさら(途中)

--- Core/Src/robot_control.cpp
+++ Core/Src/robot_control.cpp
@@ -5,27 +5,15 @@
 using namespace stm32rcos::driver;
 
 void robot_control(Motor &FR, Motor &FL, Motor &BR, Motor &BL, PS3 &ps3) {
-  Control control;
-  MekanamuController mekanamu;
-  Tire_speed Tire_speed;
+
   while (true) {
     ps3.update();
-/*
-    control.x = 0.5 * ps3.get_axis(PS3Axis::RIGHT_X);
-    control.y = 0.5 * ps3.get_axis(PS3Axis::RIGHT_Y);
-    if (ps3.get_key(PS3Key::R1)) {
-      control.turnspeed = 0.3;
-    } else if (ps3.get_key(PS3Key::L1)) {
-      control.turnspeed = -0.3;
-    } else {
-      control.turnspeed = 0;
-    }
-      */
-     
-    Tire_speed = mekanamu.output(control);
-    FR.set_speed(Tire_speed.FR);
-    FL.set_speed(Tire_speed.FL);
-    BR.set_speed(Tire_speed.BR);
-    BL.set_speed(Tire_speed.BL);
+
+    float speed = 0.3f; 
+
+    FR.set_speed(speed);
+    FL.set_speed(speed);
+    BR.set_speed(speed);
+    BL.set_speed(speed);
   }
 }
